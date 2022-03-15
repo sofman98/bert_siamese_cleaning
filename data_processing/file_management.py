67394c8e80d1defdf21e56084487f525c93a6d68
Sofiane Zeghoud <sofianezegood@gmail.com>
2022-03-15 17:41:06 +0100

    some changes

--- data_processing/file_management.py
+++ data_processing/file_management.py
@@ -8,20 +8,16 @@ import os
 def load_dataset_parquet(path='datasets/cs1_us_outlets.parquet.gzip'):
   return pd.read_parquet(path)
 
-# for saving parquet files
-def save_parquet(dataframe, path):
-  # dataframe.to_parquet(path)
-  # I prefer csv
-  dataframe.to_csv(path, index=False)
+# I prefer csv
 
 # csv
 # for loading the dataset
 def load_dataset_csv(path):
-  return pd.read_csv(path)
+  return pd.read_csv(path, sep=":")
 
 # for saving csv files
 def save_csv(dataframe, path):
-  dataframe.to_csv(path, index=False)
+  dataframe.to_csv(path, sep=':', index=False)
 
 # for creating folders
 
