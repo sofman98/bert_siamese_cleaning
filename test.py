7905021ec98855493435fc136b39201b7d2a2433
Sofiane Zeghoud <sofianezegood@gmail.com>
2022-03-18 18:33:25 +0100

    removed prediction rounding

--- test.py
+++ test.py
@@ -18,47 +18,46 @@ path_to_raw_test_data='datasets/test.parquet.gzip'
 save_prediction_to='results/predictions/last_prediction.csv'
 
 if __name__ == "__main__":
+  # we first load the model and test data
+  model = load_siamese_model(path_to_model)
+  
+  # we load the already generated feature similarity dataset
+  # if not generated, run train_test_split.py
+  test_data = np.load(path_to_feature_similarity_test_data)
+  # we make our predictions
+  X = test_data[:,:-1]
+  y = test_data[:, -1]
 
-    # we first load the model and test data
-    model = load_siamese_model(path_to_model)
-    
-    # we load the already generated feature similarity dataset
-    # if not generated, run train_test_split.py
-    test_data = np.load(path_to_feature_similarity_test_data)
-    # we make our predictions
-    X = test_data[:,:-1]
-    y = test_data[:, -1]
+  # we get the number of features for each input
+  num_features = int(X.shape[1] / 2)
+  print('Predicting...')
+  prediction = model.predict([X[:, :num_features], X[:, num_features:]], verbose=1)
 
-    # we get the number of features for each input
-    num_features = int(X.shape[1] / 2)
-    print('Predicting...')
-    prediction = model.predict([X[:, :num_features], X[:, num_features:]], verbose=1)
+  # we calculate the confusion_matrix
+  conf_mat = confusion_matrix(y, np.round(prediction))
+  conf_mat = np.array(conf_mat)
 
-    # we calculate the confusion_matrix
-    conf_mat = confusion_matrix(y, np.round(prediction))
-    conf_mat = np.array(conf_mat)
+  print('Saving the prediction...')
+  # we first load the raw test set
+  raw_test_data = load_parquet(path_to_raw_test_data)
+  # we generate the (outlet1, outlet2, similarity) dataset
+  pair_similarity_combinations = generate_entire_pair_similarity(raw_test_data, 'combinations')
+  # we remove the actual similarity
+  pair_similarity_combinations = pair_similarity_combinations[:, :-1]
+  # we save the the predictions as (outlet1, outlet2, predicted similarity) 
+  predicted_similarity = np.append(pair_similarity_combinations, prediction, axis=1)
+  predicted_similarity = pd.DataFrame(predicted_similarity, columns=['outlet1', 'outlet2', 'similarity'])
+  create_folder(save_prediction_to)
+  save_csv(predicted_similarity, save_prediction_to)
+  print(f'Saved the prediction to {save_prediction_to}')
 
-    print('Saving the prediction...')
-    # we first load the raw test set
-    raw_test_data = load_parquet(path_to_raw_test_data)
-    # we generate the (outlet1, outlet2, similarity) dataset
-    pair_similarity_combinations = generate_entire_pair_similarity(raw_test_data, 'combinations')
-    # we remove the actual similarity
-    pair_similarity_combinations = pair_similarity_combinations[:, :-1]
-    # we save the the predictions as (outlet1, outlet2, predicted similarity) 
-    predicted_similarity = np.append(pair_similarity_combinations, prediction, axis=1)
-    predicted_similarity = pd.DataFrame(predicted_similarity, columns=['outlet1', 'outlet2', 'similarity'])
-    create_folder(save_prediction_to)
-    save_csv(predicted_similarity, save_prediction_to)
-    print(f'Saved the prediction to {save_prediction_to}')
-
-    # we display the results
-    print('Confusion Matrix:\n',conf_mat)
-    ax = sns.heatmap(conf_mat, annot=True, cmap='Blues')
-    ax.set_title('Confusion Matrix\n\n')
-    ax.set_xlabel('\nPredicted Values')
-    ax.set_ylabel('Actual Values ')
-    ax.xaxis.set_ticklabels(['Negative','Positive'])
-    ax.yaxis.set_ticklabels(['Negative','Positive'])
-    plt.show()
-    
+  # we display the results
+  print('Confusion Matrix:\n',conf_mat)
+  ax = sns.heatmap(conf_mat, annot=True, cmap='Blues')
+  ax.set_title('Confusion Matrix\n\n')
+  ax.set_xlabel('\nPredicted Values')
+  ax.set_ylabel('Actual Values ')
+  ax.xaxis.set_ticklabels(['Negative','Positive'])
+  ax.yaxis.set_ticklabels(['Negative','Positive'])
+  plt.show()
+  
--- train_test_split.py
+++ train_test_split.py
@@ -23,46 +23,46 @@ test_features_save_path = 'datasets/feature_similarity_test.npy'
 
 
 if __name__ == "__main__":
-    # we load the dataset and the generated embeddings
-    dataset = load_parquet(path_to_dataset)
-    embeddings = np.load(path_to_embeddings)
+  # we load the dataset and the generated embeddings
+  dataset = load_parquet(path_to_dataset)
+  embeddings = np.load(path_to_embeddings)
 
-    # we split it
-    test = dataset.sample(frac=test_frac, random_state=0)
-    train = dataset.drop(test.index)
+  # we split it
+  test = dataset.sample(frac=test_frac, random_state=0)
+  train = dataset.drop(test.index)
 
-    # saving the test and train sets
-    save_parquet(train, train_data_save_path)
-    save_parquet(test, test_data_save_path)
+  # saving the test and train sets
+  save_parquet(train, train_data_save_path)
+  save_parquet(test, test_data_save_path)
 
-    # we add the embeddings
-    test[f'{feature}_embedding'] = list(embeddings[test.index])
-    train[f'{feature}_embedding'] = list(embeddings[train.index])
+  # we add the embeddings
+  test[f'{feature}_embedding'] = list(embeddings[test.index])
+  train[f'{feature}_embedding'] = list(embeddings[train.index])
 
-    # we reset the index not to be out of bounds
-    test.reset_index(drop=True, inplace=True)
-    train.reset_index(drop=True, inplace=True)
+  # we reset the index not to be out of bounds
+  test.reset_index(drop=True, inplace=True)
+  train.reset_index(drop=True, inplace=True)
 
-    # next we generate the feature similarity dataset (feature_set1, feature_set2, similarity)
-    # max_neg=True, which means it includes all possible combinations 
-    # in a same persistent_cluster for negative values (non-similarity)
-    # we select all possible permutations or combinations for the training
-    # and all unique combinations for the test
+  # next we generate the feature similarity dataset (feature_set1, feature_set2, similarity)
+  # max_neg=True, which means it includes all possible combinations 
+  # in a same persistent_cluster for negative values (non-similarity)
+  # we select all possible permutations or combinations for the training
+  # and all unique combinations for the test
 
-    _ = generate_feature_similarity_dataset(
-        train,
-        kind=train_kind,
-        NUM_NEG=train_NUM_NEG,
-        features=[f'{feature}_embedding'],
-        max_neg=train_max_neg,
-        save_to=train_features_save_path
-    )
+  _ = generate_feature_similarity_dataset(
+      train,
+      kind=train_kind,
+      NUM_NEG=train_NUM_NEG,
+      features=[f'{feature}_embedding'],
+      max_neg=train_max_neg,
+      save_to=train_features_save_path
+  )
 
-    _ = generate_feature_similarity_dataset(
-        test,
-        kind='combinations',
-        NUM_NEG=None,
-        features=[f'{feature}_embedding'],
-        max_neg=True,
-        save_to=test_features_save_path
-    )
+  _ = generate_feature_similarity_dataset(
+      test,
+      kind='combinations',
+      NUM_NEG=None,
+      features=[f'{feature}_embedding'],
+      max_neg=True,
+      save_to=test_features_save_path
+  )
